27d63df120828d6ebc84469b8a79c7f40485f006
tsang0607 <smartcraft.ai.hk@gmail.com>
2025-08-09 01:55:03 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: "Prompta Proxy" };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh-HK">
+      <body>{children}</body>
+    </html>
+  );
+}
